4d03decac600a824a668bb1294e7e5cd543c9b9b
Markus Guertler <mguertler@suse.com>
2015-09-30 18:36:09 +0000

    Adjusted deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git pull
 SRCDIR=~/AWS-VPC-move-IP
 CERT=~/AWS/SUSEHA2014.pem
-HOSTS="node1 node2"
+HOSTS="suse-node21 suse-node22"
 for host in $HOSTS; do
 	scp -i $CERT $SRCDIR/aws-vpc-move-ip ec2-user@$host:/tmp
 	ssh -i $CERT ec2-user@$host "sudo mv /tmp/aws-vpc-move-ip /usr/lib/ocf/resource.d/suse"
